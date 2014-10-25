c53d2aaa0d04fd61af12bf613138066f97bd2982
ppaanngggg <ppaanngggg@gmail.com>
2014-10-25 21:41:27 +0800

    add hog

---
+++ hog.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+def hog(image,cell_size,block_size,row_num,col_num,bar):
+    if len(image.shape)==3:
+        image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    image=cv2.medianBlur(image,3)
+    image=cv2.equalizeHist(image)
+    grad_x=cv2.Sobel(image,cv2.CV_32F,1,0)
+    grad_x=cv2.resize(grad_x,(col_num*cell_size,row_num*cell_size))
+    grad_y=cv2.Sobel(image,cv2.CV_32F,0,1)
+    grad_y=cv2.resize(grad_y,(col_num*cell_size,row_num*cell_size))
+    norm_vec=np.sqrt(grad_x**2+grad_y**2)
+    norm_angle=np.arctan(grad_y/(grad_x+10**-10))
+    for row in range(norm_angle.shape[0]):
+        for col in range(norm_angle.shape[1]):
+            if norm_angle[row,col]<0:
+                norm_angle[row,col]+=np.pi
+    norm_angle=np.uint16(norm_angle/(np.pi/bar))
+    cell_table=np.zeros((row_num,col_num,bar))
+    for row in range(row_num):
+        for col in range(col_num):
+            for cell_row in range(row*cell_size,(row+1)*cell_size):
+                for cell_col in range(col*cell_size,(col+1)*cell_size):
+                    cell_table[row,col,norm_angle[cell_row,cell_col]]+=norm_vec[cell_row,cell_col]
+            cell_table[row,col,0]=cell_table[row,col,8]=(cell_table[row,col,0]+cell_table[row,col,8])/2
+    # print cell_table
+    # cv2.imshow('image',norm_vec/np.max(norm_vec))
+    # cv2.waitKey()
+    hog_vec=[]
+    for row in range(row_num-block_size+1):
+        for col in range(col_num-block_size+1):
+            block_vec=[]
+            for block_row in range(row,row+block_size):
+                for block_col in range(col,col+block_size):
+                    block_vec.append(cell_table[block_row,block_col])
+            block_vec=np.hstack(block_vec)
+            block_vec=block_vec/np.sqrt(np.sum(block_vec**2))
+            hog_vec.append(block_vec)
+    hog_vec=list(np.hstack(hog_vec))
+    return hog_vec
--- segmentation.py
+++ segmentation.py
@@ -2,18 +2,32 @@ import cv2
 import numpy as np
 import skimage.segmentation as skseg
 import skimage.io as skio
+import hog
 
-img=cv2.imread('train_pic/img.jpg')
-segments=skseg.slic(img,img.shape[0]*img.shape[1]/50/50,50)
-value_set=set()
+img = cv2.imread('train_pic/img.jpg')
+segments = skseg.slic(img, img.shape[0] * img.shape[1] / 50 / 50, 60)
+skio.imshow(skseg.mark_boundaries(img,segments))
+# skio.show()
+value_set = set()
 for row in segments:
     for pixel in row:
         value_set.add(pixel)
-segment_list=[np.zeros((img.shape[0],img.shape[1]),np.uint8)for i in range(len(value_set))]
+segment_list = [np.zeros((img.shape[0], img.shape[1]), np.uint8) for i in range(len(value_set))]
 for row in range(segments.shape[0]):
     for col in range(segments.shape[1]):
-        segment_list[segments[row,col]][row,col]=255
-# for segment in segment_list:
-#     skio.imshow(segment)
-#     skio.show()
-cv2.findContours(segment_list[0],cv2.)
+        segment_list[segments[row, col]][row, col] = 255
+
+contour_list = cv2.findContours(segment_list[0], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+contour_max = \
+    [contour for contour in contour_list[1] if contour.shape[0] == max([tmp.shape[0] for tmp in contour_list[1]])][0]
+tmp_img=np.zeros((img.shape[0], img.shape[1]), np.uint8)
+for point in contour_max:
+    loc=point[0]
+    tmp_img[loc[1],loc[0]]=255
+rect=cv2.boundingRect(contour_max)
+print rect
+hog_img=np.copy(img[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]])
+skio.imshow(hog_img)
+# skio.show()
+
+hog.hog(hog_img,8,2,4,4,9)
