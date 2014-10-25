8317dbb7c74341a8d985a8ebbae8145969d50457
ppaanngggg <ppaanngggg@gmail.com>
2014-10-25 16:50:47 +0800

    use slic

--- segmentation.py
+++ segmentation.py
@@ -1,26 +1,19 @@
 import cv2
 import numpy as np
-image=cv2.imread('train_pic/img.jpg')
-image_height,image_width,image_channels=image.shape
-num_superpixels=image_height*image_width/70/70
-num_levels=4
-seed=cv2.ximgproc.createSuperpixelSEEDS(
-    image_width,
-    image_height,
-    image_channels,
-    num_superpixels,
-    num_levels
-)
-converted_img=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-num_iterations=5
-seed.iterate(converted_img,num_iterations)
-labels=seed.getLabels()
-labels_show=labels/float(np.max(labels))
-cv2.imshow('labels',labels_show)
-label_contour=seed.getLabelContourMask()
-for i in range(image_width):
-    for j in range(image_height):
-        if label_contour[i,j]:
-            image[i,j]=(0,0,255)
-cv2.imshow('contours',image)
-cv2.waitKey()
+import skimage.segmentation as skseg
+import skimage.io as skio
+
+img=cv2.imread('train_pic/img.jpg')
+segments=skseg.slic(img,img.shape[0]*img.shape[1]/50/50,50)
+value_set=set()
+for row in segments:
+    for pixel in row:
+        value_set.add(pixel)
+segment_list=[np.zeros((img.shape[0],img.shape[1]),np.uint8)for i in range(len(value_set))]
+for row in range(segments.shape[0]):
+    for col in range(segments.shape[1]):
+        segment_list[segments[row,col]][row,col]=255
+# for segment in segment_list:
+#     skio.imshow(segment)
+#     skio.show()
+cv2.findContours(segment_list[0],cv2.)
