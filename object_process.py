a80f49a85fb83fa1dd7a4e95c39f7ae31697f03c
ppaanngggg <ppaanngggg@gmail.com>
2014-11-12 08:40:23 +0800

    no message

--- object_process.py
+++ object_process.py
@@ -3,41 +3,30 @@ import cv2
 import copy
 from pymongo import MongoClient
 import train
+import threading
 
 
 class ObjectProcess:
     def __init__(self, path, clf_fore, clf_shape):
         str_list = path.split('/')
-        self.kind = str_list[1]
-        self.name = str_list[2]
+        self.kind = str_list[-2]
+        self.name = str_list[-1]
+
+        self.clf_fore = clf_fore
+        self.clf_shape = clf_shape
 
         self.image = cv2.imread(path)
-        self.image = cv2.resize(self.image,(400,400))
+        self.image = cv2.resize(self.image, (400, 400))
 
-        self.image_proc_s = ImageProcess(self.image, 70)
-        self.image_proc_s.set_classify_target_list(
-            clf_fore.predict(
-                self.image_proc_s.get_classify_vec_list()
-            )
-        )
-        self.image_proc_s.compute_foreground_mask()
-        self.image_proc_s.compute_foreground_image()
-        self.image_proc_s.compute_color_hist()
-        self.image_proc_s.compute_ORB_list()
-
-        self.image_proc_m = ImageProcess(self.image, 100)
-        self.image_proc_m.set_classify_target_list(
-            clf_shape.predict(
-                self.image_proc_m.get_classify_vec_list()
-            )
-        )
-
-        self.image_proc_l = ImageProcess(self.image, 130)
-        self.image_proc_l.set_classify_target_list(
-            clf_shape.predict(
-                self.image_proc_l.get_classify_vec_list()
-            )
-        )
+        thread_s = threading.Thread(target=self.init_image_proc_s())
+        thread_m = threading.Thread(target=self.init_image_proc_m())
+        thread_l = threading.Thread(target=self.init_image_proc_l())
+        thread_s.start()
+        thread_m.start()
+        thread_l.start()
+        thread_s.join()
+        thread_m.join()
+        thread_l.join()
 
         self.hog_list = []
         hog_list = \
@@ -51,6 +40,34 @@ class ObjectProcess:
         self.client = MongoClient()
         self.db = self.client.object_finder
 
+    def init_image_proc_s(self):
+        self.image_proc_s = ImageProcess(self.image, 70)
+        self.image_proc_s.set_classify_target_list(
+            self.clf_fore.predict(
+                self.image_proc_s.get_classify_vec_list()
+            )
+        )
+        self.image_proc_s.compute_foreground_mask()
+        self.image_proc_s.compute_foreground_image()
+        self.image_proc_s.compute_color_hist()
+        self.image_proc_s.compute_ORB_list()
+
+    def init_image_proc_m(self):
+        self.image_proc_m = ImageProcess(self.image, 100)
+        self.image_proc_m.set_classify_target_list(
+            self.clf_shape.predict(
+                self.image_proc_m.get_classify_vec_list()
+            )
+        )
+
+    def init_image_proc_l(self):
+        self.image_proc_l = ImageProcess(self.image, 130)
+        self.image_proc_l.set_classify_target_list(
+            self.clf_shape.predict(
+                self.image_proc_l.get_classify_vec_list()
+            )
+        )
+
     def get_color_hist(self):
         return copy.deepcopy(self.image_proc_s.get_color_hist())
 
@@ -63,21 +80,21 @@ class ObjectProcess:
     def store_color_hist(self):
         coll = self.db.color_hist
         coll.insert({
-                'vec': self.get_color_hist(),
-                'kind': self.kind,
-                'name': self.name
+            'vec': self.get_color_hist(),
+            'kind': self.kind,
+            'name': self.name
         })
         return self
 
     def store_ORB_list(self):
         coll = self.db.ORB_list
-        ORB_list=self.get_ORB_list()
+        ORB_list = self.get_ORB_list()
         try:
             for ORB in ORB_list:
                 coll.insert({
-                        'vec': [int(num) for num in ORB],
-                        'kind': self.kind,
-                        'name': self.name
+                    'vec': [int(num) for num in ORB],
+                    'kind': self.kind,
+                    'name': self.name
                 })
         except:
             pass
@@ -85,24 +102,23 @@ class ObjectProcess:
 
     def store_hog_list(self):
         coll = self.db.hog_list
-        hog_list=self.get_hog_list()
+        hog_list = self.get_hog_list()
         try:
             for hog in hog_list:
                 coll.insert({
-                        'vec': hog,
-                        'kind': self.kind,
-                        'name': self.name
+                    'vec': hog,
+                    'kind': self.kind,
+                    'name': self.name
                 })
         except:
             pass
         return self
 
-    def write_fore_image(self,img_base):
+    def write_fore_image(self, img_base):
         cv2.imwrite(
-            img_base+'/'+self.kind+'/'+self.name,
+            img_base + '/' + self.kind + '/' + self.name,
             self.image_proc_s.get_foreground_image()
         )
-
 
 
 def test():
