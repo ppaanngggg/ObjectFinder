289969a2946637ebc2fbdd436f846e8d95ef9037
ppaanngggg <ppaanngggg@gmail.com>
2014-11-15 22:52:51 +0800

    no message

---
+++ BPNN.py
@@ -0,0 +1,245 @@
+# coding=utf-8
+import math
+import random
+
+
+class BpnnNode:
+    '''
+    神经元节点
+    '''
+    def __init__(self, input_num):
+        '''
+        :param input_num:输入数量
+        :return:None
+        '''
+        self.input = [1.0] * (input_num + 1)
+        self.weight = [0.0] * (input_num + 1)
+        self.old_weight = [0.0] * (input_num + 1)
+        for i in range(0, len(self.weight)):
+            self.weight[i] = self.rand(-0.05, 0.05)
+        self.delta = 0.0
+        self.output = 0.0
+
+    def rand(self,a, b):
+        '''
+        :param a:下界
+        :param b:上界
+        :return:随机值
+        '''
+        return (b-a)*random.random() + a
+
+    def sigmoid(self, x):
+        '''
+        阈值函数
+        '''
+        return 1.0 / (1.0 + math.e ** -x)
+        # return math.tanh(x)
+
+    def dsigmoid(self, y):
+        """
+        阈值函数倒数
+        """
+        return y * (1.0 - y)
+        # return 1.0 - y**2
+
+    def compute(self, data):
+        '''
+        计算输出值
+        :param data:输入数据
+        :return:None
+        '''
+        if len(data) != len(self.input) - 1:
+            raise ValueError('wrong length of input for node!')
+        for i in range(len(self.input) - 1):
+            self.input[i] = data[i]
+        net = 0.0
+        for i in range(len(self.input)):
+            net += self.input[i] * self.weight[i]
+        self.output = self.sigmoid(net)
+
+    def update(self, weight_list, delta_list, eta, momentum):
+        '''
+
+        :param weight_list:
+        :param delta_list:
+        :param eta:
+        :param momentum:
+        :return:
+        '''
+        self.delta = 0.0
+        for i in range(len(weight_list)):
+            self.delta += weight_list[i] * delta_list[i]
+        self.delta *= self.dsigmoid(self.output)
+        for i in range(len(self.weight)):
+            change = eta * self.delta * self.input[i]+momentum*(self.weight[i]-self.old_weight[i])
+            # change= eta * self.delta * self.input[i]
+            self.old_weight[i]=self.weight[i]
+            self.weight[i] += change
+
+
+class Bpnn:
+    def __init__(self, input_num, node_num_list):
+        '''
+
+        :param input_num:
+        :param node_num_list:
+        :return:
+        '''
+        self.bpnn = []
+        self.input_num = input_num
+        tmp_input_num = input_num
+        for node_num in node_num_list:
+            self.bpnn.append([BpnnNode(tmp_input_num) for i in range(node_num)])
+            tmp_input_num = node_num
+
+    def compute(self, input):
+        '''
+
+        :param input:
+        :return:
+        '''
+        if len(input) != self.input_num:
+            raise ValueError('wrong length of input for bpnn!')
+        tmp_input = input
+        for node_list in self.bpnn:
+            for node in node_list:
+                node.compute(tmp_input)
+            tmp_input = [node.output for node in node_list]
+
+    def output(self):
+        '''
+
+        :return:
+        '''
+        return [node.output for node in self.bpnn[-1]]
+
+    def error(self, example_list):
+        '''
+
+        :param example_list:
+        :return:
+        '''
+        error = []
+        for example in example_list:
+            self.compute(example[0])
+            output = self.output()
+            target = example[1]
+            e = 0.
+            for i in range(len(output)):
+                e += (target[i] - output[i]) ** 2
+            error.append(e / 2.)
+        return error
+
+    def update(self, target, eta, momentum):
+        '''
+
+        :param target:
+        :param eta:
+        :param momentum:
+        :return:
+        '''
+        if len(target) != len(self.bpnn[-1]):
+            raise ValueError('wrong length of target for bpnn!')
+        for i in range(len(self.bpnn[-1])):
+            self.bpnn[-1][i].update([1.0], [target[i] - self.bpnn[-1][i].output], eta, momentum)
+        tmp_list = range(len(self.bpnn) - 1)
+        tmp_list.reverse()
+        for i in tmp_list:
+            delta_list = [node.delta for node in self.bpnn[i + 1]]
+            for j in range(len(self.bpnn[i])):
+                weight_list = [node.old_weight[j] for node in self.bpnn[i + 1]]
+                self.bpnn[i][j].update(weight_list, delta_list, eta, momentum)
+
+    def train(self, example_list, error, eta=0.4, momentum=0.3):
+        '''
+
+        :param example_list:
+        :param error:
+        :param eta:
+        :param momentum:
+        :return:
+        '''
+        error_now=sum(self.error(example_list))
+        while  error_now> error:
+            # print '\n----------\n'
+            for example in example_list:
+                self.compute(example[0])
+                # print self.output()
+                self.update(example[1], eta,momentum)
+            error_now=sum(self.error(example_list))
+            print error_now
+
+    def debug_input(self):
+        '''
+
+        :return:
+        '''
+        print 'debug input'
+        for i in range(len(self.bpnn)):
+            print '\t', 'layer ', i
+            for node in self.bpnn[i]:
+                print '\t\t', node.input
+
+    def debug_output(self):
+        '''
+
+        :return:
+        '''
+        print 'debug output'
+        for i in range(len(self.bpnn)):
+            # print '\t', 'layer ', i
+            print '\t\t', [node.output for node in self.bpnn[i]]
+
+    def debug_delta(self):
+        '''
+
+        :return:
+        '''
+        print 'debug delta'
+        for i in range(len(self.bpnn)):
+            print '\t', 'layer ', i
+            for node in self.bpnn[i]:
+                print '\t\t', node.delta
+
+    def debug_weight(self):
+        '''
+
+        :return:
+        '''
+        print 'debug weight'
+        for i in range(len(self.bpnn)):
+            print '\t', 'layer ', i
+            for node in self.bpnn[i]:
+                print '\t\t', node.weight
+
+    def debug_train(self, example_list, times, eta=0.4, momentum=0.3):
+        '''
+
+        :param example_list:
+        :param times:
+        :param eta:
+        :param momentum:
+        :return:
+        '''
+        for i in range(times):
+            # print '\n- - - - - - - - - - - - - - -\n'
+            for example in example_list:
+                self.compute(example[0])
+                # print self.output()
+                self.update(example[1], eta,momentum)
+            error_now=sum(self.error(example_list))
+            print error_now
+
+# example_list = [
+#     [[0.,0.,0.],[0]],
+#     [[0.,0.1,0.],[0]],
+#     [[0.1,0.,0.],[0]],
+#     [[0.,0.1,0.1],[1]],
+#     [[0.1,0.,0.1],[1]],
+#     [[0.1,0.1,0.0],[0]]
+# ]
+# bpnn = Bpnn(3, [1])
+# # bpnn.train(example_list, 0.001)
+# bpnn.debug_train(example_list,10000)
+# # bpnn.compute([0.9,0.9])
+# print bpnn.output()
--- arg_learn.py
+++ arg_learn.py
@@ -1,23 +1,82 @@
 from path_process import PathProcess
 from object_process import ObjectProcess
 import train
+from pymongo import MongoClient
+from random import *
+import numpy as np
+import BPNN
 
-def main():
+def store_train_find_sample():
     clf_fore = train.train_sample('fore')
     clf_shape = train.train_sample('shape')
+
+    client = MongoClient()
+    db = client.object_finder
+    coll = db.train_find
 
     path_proc = PathProcess('train_pic_arg')
     for kind in path_proc.get_kind_list():
         for path in path_proc.get_file_path_list(kind):
-            print path
-            obj_proc=ObjectProcess(path,clf_fore,clf_shape)
-            print obj_proc.get_fit_color_dict()
-            print obj_proc.get_best_fit_color_dict()
-            # obj_proc.get_fit_ORB_dict()
-            # obj_proc.get_best_fit_ORB_dict()
-            # obj_proc.get_fit_hog_dict()
-            # obj_proc.get_best_fit_hog_dict()
+            # print path
+            obj_proc = ObjectProcess(path, clf_fore, clf_shape)
+            coll.insert({
+                'kind': obj_proc.kind,
+                'name': obj_proc.name,
+                'color': obj_proc.get_fit_color_dict(),
+                'best_color': obj_proc.get_best_fit_color_dict(),
+                'ORB': obj_proc.get_fit_ORB_dict(),
+                'best_ORB': obj_proc.get_best_fit_ORB_dict(),
+                'hog': obj_proc.get_fit_hog_dict(),
+                'best_hog': obj_proc.get_best_fit_hog_dict()
+            })
+
+
+def load_train_find_sample():
+    client = MongoClient()
+    db = client.object_finder
+    coll = db.train_find
+
+    train_find_list = []
+    for find in coll.find():
+        train_find_list.append(find)
+    return train_find_list
+
+
+def train_arg():
+    train_find_list = load_train_find_sample()
+    vec_list = []
+    target_list = []
+    for train_find in train_find_list:
+        vec = []
+        for i in ('color', 'best_color', 'ORB', 'best_ORB', 'hog', 'best_hog'):
+            for j in ('cloth', 'cup', 'shore'):
+                try:
+                    vec.append(sum(train_find[i][j].values()))
+                except:
+                    vec.append(0)
+        for i in range(0, len(vec), 3):
+            s = sum(vec[i:i + 3])
+            for j in range(i, i + 3):
+                vec[j] /= float(s)
+        vec_list.append(vec)
+        target = []
+        for i in ('cloth', 'cup', 'shore'):
+            if train_find['kind'] == i:
+                target.append(1)
+            else:
+                target.append(0)
+        target_list.append(target)
+
+    bpnn=BPNN.Bpnn(len(vec_list[0]),[3])
+    sample_list=[]
+    for i in range(len(vec_list)):
+        sample_list.append([vec_list[i],target_list[i]])
+    bpnn.train(sample_list,0.1)
+    for vec in vec_list:
+        bpnn.compute(vec)
+        print bpnn.output()
 
 
 if __name__ == '__main__':
-    main()
+    # store_train_find_sample()
+    train_arg()
